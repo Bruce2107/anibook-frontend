f01ffbfdfb40a02e7a1f8e6b71cbec9bf745cca9
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-04-29 14:21:12 -0300

    redux removido

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,15 @@ import usePersistedState from './utils/usePersistedState';
 import Navbar from './components/Navbar';
 
 function App() {
-  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
-  const toggleTheme = () => setTheme(theme.title === 'light' ? dark : light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark);
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <>
-        <Navbar toggleTheme={toggleTheme} />
-      </>
+      <Navbar toggleTheme={toggleTheme} />
     </ThemeProvider>
   );
 }
