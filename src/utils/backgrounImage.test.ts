7de7634292b4e19f5070a99c16bcb556d0b54bcc
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-05-01 19:32:01 -0300

    usePersistedState removido

--- src/utils/backgrounImage.test.ts
+++
@@ -1,9 +0,0 @@
-import getImage from './backgroundImage';
-describe('Background Image', () => {
-  test('should return a string with the name of image', () => {
-    const image = getImage();
-    expect(image).toBeDefined();
-  });
-});
-
-export default {};
