534f05c7081c0106dafcafa4d3b60fd6aaa4fb39
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-05-24 10:20:33 -0300

    document title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,11 +7,12 @@ import getUrlImage from '../../utils/getImageUrl';
 import { TypeCard } from '../../constants/Types';
 import api from '../../services/api';
 import Card from '../../components/Card';
+import setPageTitle from '../../utils/setPageTitle';
 
 export default function Home() {
   const [cards, setCards] = useState<Array<TypeCard>>();
   useEffect(() => {
-    document.title = 'Home';
+    setPageTitle('Home');
     api.get('anime/card/random?limit=3').then((res: AxiosResponse<{ data: Array<TypeCard> }>) => {
       setCards(res.data.data);
     });
---
+++ src/utils/setPageTitle.ts
@@ -0,0 +1,3 @@
+export default function (title: string) {
+  document.title = title;
+}
