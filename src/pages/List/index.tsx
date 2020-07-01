e58c8f0a423bb05a07a42be34e9cc73ae3713348
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-07-01 17:02:13 -0300

    change request route

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -35,7 +35,7 @@ export default function List({ pageName, type, limitPerPage }: Props) {
   useEffect(() => {
     setPageTitle(pageName);
     api
-      .get(`/${type}/card/sort?order=name`)
+      .get(`/${type}/card/sort/name`)
       .then((res: AxiosResponse<{ data: Array<TypeCard>; rows: number }>) => {
         setCards(res.data.data);
         setTotalRows(res.data.rows);
